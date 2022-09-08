1f5648b730ed4461cbebb841125d3f11991f593f
csiki <chickenhun@gmail.com>
2022-09-09 01:51:45 +0200

    spaceships in atom

--- spaceshipify.py
+++ spaceshipify.py
@@ -1,4 +1,7 @@
 import sys
+import io
+import argparse
+import re
 import numpy as np
 from scipy import signal
 import matplotlib.pyplot as plt
@@ -8,6 +11,7 @@ from skimage.transform import resize
 from augment import fit_to_box, fill_inside_contour
 from matplotlib.pyplot import imread, imsave, imshow
 from stable_diff import StableSpaceship
+import ast
 
 
 def code_outline(code, tolerate_0s=False):
@@ -87,7 +91,8 @@ def draw_contour(left, right, char_w, dilate, pad, fill=True):  # left and right
     return np.tile(np.expand_dims(cont_img, -1), (1, 1, 3))
 
 
-def code2contour(code, box_size=256, order=3, dilate=1, pad=5, fill_cont=True, set_left=None):
+def code2contour(code, box_size=256, order=3, dilate=1, pad=5, fill_cont=True, set_left=None,
+                 do_fit_to_box=True, ret_pix_sizes=False):
 
     left_cont, right_cont = code_outline(code)
     line_h = box_size // len(right_cont)
@@ -114,8 +119,11 @@ def code2contour(code, box_size=256, order=3, dilate=1, pad=5, fill_cont=True, s
     # orig_shape = contour.shape  # keep for later so the generated ship can be deformed back, if needed at all
 
     # contour = (resize(contour, (box_size, box_size)) * 255).astype(np.uint8)
-    contour = fit_to_box(contour, box_size, box_size, np.array([0, 0, 0]))
+    if do_fit_to_box:
+        contour = fit_to_box(contour, box_size, box_size, np.array([0, 0, 0]))
 
+    if ret_pix_sizes:
+        return contour, (line_h, char_w)
     return contour
 
 
@@ -138,7 +146,7 @@ def fit_to_box(img, h, w, fill_col):  # img has to be uint8
 
 CODE2 = """
 def code_contour(code, tolerate_0s=False):
-    lines = code.split('\n')
+    lines = code.split('\\n')
 
     left, right = [], []
     for l, line in enumerate(lines):
@@ -163,6 +171,18 @@ def code_contour(code, tolerate_0s=False):
         right.append(r)
 
     return left, right
+
+
+def code_contour(code, tolerate_0s=False):
+    lines = code.split('\\n')
+
+    left, right = [], []
+    
+    
+    def code_contour(code, tolerate_0s=False):
+        lines = code.split('\\n')
+    
+        left, right = [], []
 """
 
 CODE3 = """
@@ -283,11 +303,8 @@ python3.6 test.py --dataroot ${dataroot_p2p} --gpu_ids 0,1 --name ${name} --load
 # TODO break the code further into blocks according to double line breaks - have it as option here
 
 
-if __name__ == '__main__':
-
-    root_dir = sys.argv[1] if len(sys.argv) > 1 else 'data/augm'
-    out_dir = f'{root_dir}/test'
-    box_size = 256
+def example_run():
+    box_size = 512
     order = 1
     dilate = 1
     pad = 10
@@ -295,7 +312,7 @@ if __name__ == '__main__':
     set_left = 1  # or None  # TODO
 
     sss = StableSpaceship()
-    steps = 200
+    steps = 250
     prompt = 'sideview of a spaceship from star wars with silver and blue colors. ' \
              'high definition aesthetic 3d model'
 
@@ -307,7 +324,110 @@ if __name__ == '__main__':
         plt.imshow(np.concatenate([resize(contour, ship.shape), ship], axis=1))
         plt.show()
 
-        # prep_contour = np.concatenate([np.zeros_like(contour), contour], axis=1)
-        # imsave(f'{out_dir}/code_{c}.jpg', prep_contour)
+        plt.imsave('contour_example.png', contour)
+        plt.imsave('spaceship_example.png', ship)
 
-    # python3.6 test.py <SAME PARAMS AS FOR train.py>
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Spaceshipifies Python scripts')
+    parser.add_argument('code_path', help='path to Python script', default='tmp.py')
+    parser.add_argument('out_path', help='destination of generated spaceship image', default='out/armada.png')
+    # parser.add_argument('--line', help='editor line height in pixels', type=int)
+    parser.add_argument('-st', '--steps', help='number of refinement steps to take', type=int, default=200)
+    parser.add_argument('-si', '--size', help='size of the output image', type=int, default=512)
+    parser.add_argument('-p', '--prompt', help='style description of spaceship', default=
+                        'high definition aesthetic sideview of a single spaceship from star wars with blue colors in empty space')
+
+    args = parser.parse_args()
+
+    # contour generation settings
+    box_size = args.size
+    order = 1
+    dilate = 1
+    pad = 0
+    fill_cont = True
+    set_left = 1  # or None
+    # line_height = args.line
+
+    # stable diffusion settings
+    prompt = args.prompt
+    steps = args.steps
+
+    # load code and find function
+    with open(args.code_path, 'rt') as f:
+        code_txt = f.read()
+
+    # # find functions w/ regex
+    # reg = re.compile(r'def (\w+)\s*\((.*?)\):')
+    # for m in reg.finditer(code_txt):
+    #     print(m.start(), m.group())
+
+    hide_stdout = sys.stdout
+    hide_stderr = sys.stderr
+    # sys.stdout = io.BytesIO()  # TODO suppress stdout
+    # sys.stderr = io.BytesIO()  # TODO suppress stderr
+    try:
+        # find functions w/ ast
+        ast_parsed = ast.parse(code_txt)
+        funs = [n for n in ast.walk(ast_parsed) if type(n) == ast.FunctionDef]
+        fun_spans = [(n.lineno, n.end_lineno) for n in funs]
+        src_segments = [ast.get_source_segment(code_txt, n, padded=True) for n in funs]
+
+        # select non-overlapping functions (nested functions don't get their own spaceship)
+        invalid_spans_i = set()
+        for fi in range(0, len(fun_spans) - 1):
+            for fj in range(fi + 1, len(fun_spans)):
+                si, sj = fun_spans[fi], fun_spans[fj]
+                if si[0] <= sj[0] and sj[1] <= si[1]:
+                    invalid_spans_i.add(fj)
+
+        fun_spans = [sp for i, sp in enumerate(fun_spans) if i not in invalid_spans_i]
+        src_segments = [ss for i, ss in enumerate(src_segments) if i not in invalid_spans_i]
+
+        if len(fun_spans) == 0:
+            print('{"liftoff": "nah", "whynah": "nofun"}', file=hide_stdout)
+            exit(0)
+
+        # load stable diffusion model
+        sss = StableSpaceship()
+
+        # generate spaceships for each function
+        contours, ships = [], []
+        pix_sizes = []  # line height and character widths calculated individually for each spaceship
+
+        for span, segment in zip(fun_spans, src_segments):
+            contour, (line_h, char_w) = code2contour(segment, box_size, order, dilate, pad, fill_cont, set_left,
+                                                     do_fit_to_box=False, ret_pix_sizes=True)
+            orig_shape = contour.shape
+            ship = sss.run(resize(contour, (box_size, box_size)), prompt=prompt, steps=steps)
+            ship = resize(ship, orig_shape)
+
+            ships.append(ship)
+            contours.append(contour)
+            pix_sizes.append([line_h, char_w])
+
+        # resize spaceships to have the same line height and char width
+        pix_sizes = np.array(pix_sizes, dtype=float)
+        shared_line_h = int(pix_sizes[:, 0].mean())
+        shared_char_w = int(pix_sizes[:, 1].mean())
+        resize_h_to = [int((span[1] - span[0] + 1) * shared_line_h) for span in fun_spans]
+        resize_w_by = shared_char_w / pix_sizes[:, 1]
+        ships = [resize(ship, [resize_h_to[i], int(ship.shape[1] * resize_w_by[i])])
+                 for i, ship in enumerate(ships)]
+
+        # combine spaceships
+        armada_h = (code_txt.count('\n') + 1 + 1) * shared_line_h  # +1 for safety
+        armada_w = max([ship.shape[1] for ship in ships])
+        armada = np.zeros((armada_h, armada_w, ships[0].shape[-1]), dtype=ships[0].dtype)
+        for ship, span in zip(ships, fun_spans):
+            h_start = (span[0] - 1) * shared_line_h  # 1 -> 0-indexing
+            h_end = span[1] * shared_line_h  # -1 as above and +1 for including the last line
+            armada[h_start:h_end, 0:ship.shape[1], :] = ship
+
+        plt.imsave(args.out_path, armada)
+        print('{"liftoff": "success", "line_h": %d, "char_w": %d}' % (shared_line_h, shared_char_w), file=hide_stdout)
+
+    except SyntaxError:  # this ain't a debugger
+        print('{"liftoff": "nah", "whynah": "syntaxerror"}', file=hide_stdout)
+        exit(1)
